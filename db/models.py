3b231103887cb2058073e75e1f9a1959ee71377a
bunnyxt <bunnyxt@outlook.com>
2019-10-16 15:13:19 +0800

    Change db model and add related db operations.

--- db/models.py
+++ db/models.py
@@ -11,6 +11,7 @@ class Video(Base):
 
     aid = Column(Integer, primary_key=True, nullable=False, unique=True)
     tid = Column(Integer, nullable=False)
+    create = Column(Integer, nullable=False)
 
     def __repr__(self):
-        return "<Video(aid=%d, tid=%s)>" % (self.aid, self.tid)
+        return "<Video(aid=%d, tid=%d, create=%d)>" % (self.aid, self.tid, self.create)
--- db/operation.py
+++ db/operation.py
@@ -1,7 +1,35 @@
+from sqlalchemy.exc import IntegrityError as SqlalchemyIntegrityError
+from pymysql.err import IntegrityError as PymysqlIntegrityError
+from sqlalchemy.exc import InvalidRequestError
+from .models import Video
+from sqlalchemy import func
+
 __all__ = ['DBOperation']
 
 
 class DBOperation:
+
+    @classmethod
+    def add(cls, obj, session):
+        try:
+            session.add(obj)
+            session.commit()
+        except Exception as e:
+            print(e)
+            session.rollback()
+
+    @classmethod
+    def add_all(cls, obj_list, session):
+        try:
+            session.add_all(obj_list)
+            session.commit()
+        except (SqlalchemyIntegrityError, PymysqlIntegrityError, InvalidRequestError):
+            session.rollback()
+            for obj in obj_list:
+                cls.add(obj, session)
+        except Exception as e:
+            print(e)
+            session.rollback()
 
     @classmethod
     def query_all(cls, table, session):
@@ -13,19 +41,37 @@ class DBOperation:
             return None
 
     @classmethod
-    def add(cls, obj, session):
+    def count_video_via_tid(cls, tid, session):
         try:
-            session.add(obj)
-            session.commit()
+            count = session.query(func.count(Video.aid)).filter(Video.tid == tid).scalar()
+            return count
         except Exception as e:
             print(e)
+            return None
 
     @classmethod
-    def add_all(cls, obj_list, session):
+    def query_last_x_aid_via_tid(cls, tid, x, session):
         try:
-            session.add_all(obj_list)
-            session.commit()
+            result = session.query(Video).filter(Video.tid == tid).order_by(Video.create.desc()).limit(x).all()
+            return result
         except Exception as e:
             print(e)
-            for obj in obj_list:
-                cls.add(obj, session)
+            return None
+
+    @classmethod
+    def query_video_via_aid(cls, aid, session):
+        try:
+            result = session.query(Video).filter(Video.aid == aid).first()
+            return result
+        except Exception as e:
+            print(e)
+            return None
+
+    @classmethod
+    def count_later_video_via_tid_and_create(cls, tid, create, session):
+        try:
+            count = session.query(func.count(Video.aid)).filter(Video.tid == tid, Video.create >= create).scalar()
+            return count
+        except Exception as e:
+            print(e)
+            return None
